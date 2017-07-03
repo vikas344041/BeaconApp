a1dc78fbd26116ecfaa3b1fcc30665e11b1d5c8a
vikas344041 <vikas.khinchi@gmail.com>
2017-07-03 03:22:03 +0200

    Some major changes

--- Beacons/app/src/main/java/com/example/dvg_vk/beacons/BackgroundSubscribeIntentService.java
+++ Beacons/app/src/main/java/com/example/dvg_vk/beacons/BackgroundSubscribeIntentService.java
@@ -12,14 +12,6 @@ import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.dvg_vk.beacons.DAO.RequestHandler;
 
 import org.altbeacon.beacon.Beacon;
